71a2f042cb69fc8f4db44b133dbcc55bbd94fad9
kazukisekiduka <kazukisekiduka@gmail.com>
2019-05-31 15:34:49 +0900

    コメントｺﾒﾝﾄ

---
+++ testPython.py
@@ -0,0 +1,39 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+FROM_ADDRESS = 'kyabetumiso@gmail.com'
+MY_PASSWORD = 'Seki1555@@'
+TO_ADDRESS = 'kazukisekiduka@gmail.com'
+# BCC = 'receiver2@test.net'
+SUBJECT = 'Hirokuからあなたへ'
+BODY = '届いた？'
+
+
+def create_message(from_addr, to_addr, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Date'] = formatdate()
+    return msg
+
+
+def send(from_addr, to_addrs, msg):
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.ehlo()
+    smtpobj.starttls()
+    smtpobj.ehlo()
+    smtpobj.login(FROM_ADDRESS, MY_PASSWORD)
+    smtpobj.sendmail(from_addr, to_addrs, msg.as_string())
+    smtpobj.close()
+
+
+if __name__ == '__main__':
+
+    to_addr = TO_ADDRESS
+    subject = SUBJECT
+    body = BODY
+
+    msg = create_message(FROM_ADDRESS, to_addr, subject, body)
+    send(FROM_ADDRESS, to_addr, msg)
